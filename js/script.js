f79f850f5fa4b07bd97e4793f40699186a92b505
Charles Wooley <cwooley89@gmail.com>
2017-07-20 13:34:02 -0400

    Done.

--- js/script.js
+++ js/script.js
@@ -1,20 +1,21 @@
 $(document).ready(function(){
-  
-  // browser refreshes on submit
-  $('form').on('submit', function(){
-    var name = $('#name').val();
-    $("#hello").text("Hello, " + name);
-  });
 
-  //examine event object
-  // $('form').on('submit', function(event){
+  // // browser refreshes on submit
+  // $('form').on('submit', function(){
   //   var name = $('#name').val();
   //   $("#hello").text("Hello, " + name);
-  //   debugger;
-  //   event.preventDefault();
   // });
+  //
 
-  // stop page refresh
+
+  //examine event object
+  $('form').on('submit', function(event){
+    var name = $('#name').val();
+    $("#hello").text("Hello, " + name);
+    event.preventDefault();
+  });
+
+  // //stop page refresh
   // $('form').on('submit', function(event){
   //   var name = $('#name').val();
   //   $("#hello").text("Hello, " + name);
